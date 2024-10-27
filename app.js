10a51ab6fc22bf6500e19fb1a0c3580fcef36b0e
Ahmed-Rakha <rakhaahmed1@gmail.com>
2024-10-28 00:12:09 +0300

    add origin cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: ["https://spain-fixed-team-frontend.vercel.app","http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
